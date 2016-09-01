30f74cd1376f4751de54c19ea4c2380f554f1350
Philipp Schmitt <philipp@schmitt.co>
2016-09-01 14:30:56 +0200

    Post install setup process

--- install.sh
+++ install.sh
@@ -23,20 +23,38 @@ else
     exit 4
 fi
 
-
 # Install the systemd service file (only if running systemd)
-if pidof systemd > /dev/null
+# if pidof systemd > /dev/null
+if [[ -e /etc/systemd/system ]]
 then
     if cp "$DEST/nastyrhum.service" /etc/systemd/system
     then
-        echo "Systemd service installed"
+        # Edit the service file if required
+        if [[ "$DEST" != /srv/nastyrhum ]]
+        then
+            sed -i "s|/srv/nastyrhum|$DEST|g" /etc/systemd/system/nastyrhum.service
+        fi
+        echo "The systemd service has been installed (nastyrhum.service)"
     else
         echo "Failed to install systemd service" >&2
     fi
 fi
 
+echo
+echo "SETUP"
+read -rp "VPN Provider: " vpn_provider
+read -rp "VPN Account Username: " vpn_username
+read -rsp "VPN Account Password: " vpn_password; echo
+read -rp "VPN Exit Point: " vpn_exit_point
 
+sed "s|\(OPENVPN_PROVIDER\)=.*|\1=$vpn_provider|;
+     s|\(OPENVPN_CONFIG\)=.*|\1=$vpn_exit_point|;
+     s|\(OPENVPN_USERNAME\)=.*|\1=$vpn_username|;
+     s|\(OPENVPN_PASSWORD\)=.*|\1=$vpn_password|" \
+     "$DEST/docker-compose.credentials.yml.sample" > \
+     "$DEST/docker-compose.credentials.yml"
+
+echo
 echo "Setup complete!"
 echo
-echo "Now please create $DEST/docker-compose.credentials.yml to configure your VPN settings"
-echo "# cp -a $DEST/docker-compose.credentials.yml.sample $DEST/docker-compose.credentials.yml"
+echo "You can edit your credentials here: $DEST/docker-compose.credentials.yml"
